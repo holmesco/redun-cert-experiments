70ae3e16b15f159a4e0f06ecd779fb86980432bc
cholmes <connor.holmes@mail.utoronto.ca>
2026-07-02 15:09:47 +0000

    Add RANSAC function and parameters. Tests working but certifier broken.

--- src/stereo_loc/DataAssociationBlocks.py
+++ src/stereo_loc/DataAssociationBlocks.py
@@ -24,18 +24,18 @@ class DataAssociationConfig:
     method: DataAssociationMethod = DataAssociationMethod.CLIPPER
     # Device
     default_device: str = "cpu"
-    
-    # --- Graph Definition Parameters ---   
+
+    # --- Graph Definition Parameters ---
     invariant_epsilon: float = 0.3  # 30 cm, correspoding to max allowable discrepancy
     invariant_sigma: float = (
         0.15  # 15 cm, half the max allowable discrepancy, to get a good scoring function
     )
     # Threshold for converting to unweighted graph. (zero if not converting)
     unweighted: bool = False
-    
+
     # CLIPPER rounding strategy
     clipper_rounding_method = clipperpy.Rounding.DSD_HEU
-    
+
     # --- Certification Parameters ---
     # Certification flag for data association
     certify: bool = False
@@ -45,28 +45,54 @@ class DataAssociationConfig:
     )
     # Rank ratio for determining rank of SDP solution. Eigenvalue considered to be zero if it is less than rank_ratio * max_eigenvalue. This is used to determine if the SDP solution is rank-1.
     rank_ratio: float = 1e-6
-    
+
     # --- Clique to Solution Conversion Parameters ---
     # inlier to solution conversion iterations
     clique_to_solution_iters: int = 100
     # inlier to solution tolerance
     clique_to_solution_tol: float = 1e-9
-    
+
     # --- RANSAC parameters ---
     # number of points to use for RANSAC pose estimation
     ransac_num_sample_pts: int = 3
     # number of RANSAC iterations
     ransac_num_iterations: int = 50
-    
+    # inlier threshold for RANSAC pose estimation (in meters)
+    # NOTE: This should be set to the same value as the 0.5x invariant_epsilon parameter for RANSAC to generate cliques of the graph.
+    ransac_inlier_threshold: float = 0.15
+
+
 class DataAssociationBlock:
     """Data association block that takes in two sets of 3D keypoints and outputs a set of matched keypoints.
 
-    Uses CLIPPER for 3D data association: takes in two sets of 3D keypoints and outputs a set of matched keypoints."""
+    Uses CLIPPER for 3D data association: takes in two sets of 3D keypoints and outputs a set of matched keypoints.
+    """
 
     def __init__(self, config: DataAssociationConfig):
+        # Store config
         self.config = config
+        # Check RANSAC parameters
+        if self.config.method == DataAssociationMethod.RANSAC:
+            assert (
+                self.config.ransac_num_sample_pts >= 3
+            ), "RANSAC requires at least 3 points for pose estimation."
+            assert (
+                self.config.ransac_num_iterations > 0
+            ), "RANSAC requires at least 1 iteration."
+            assert (
+                self.config.ransac_inlier_threshold > 0
+            ), "RANSAC requires a positive inlier threshold."
+            if (
+                self.config.ransac_inlier_threshold
+                > 0.5 * self.config.invariant_epsilon
+            ):
+                print(
+                    "Warning: RANSAC inlier threshold is greater than 0.5x invariant epsilon. This may lead to RANSAC inliers that are not cliques of the data association graph."
+                )
+
         # Affinity matrix
         self.M: np.ndarray | None = None
+        self.M_torch: torch.Tensor | None = None
         # Set up invariant
         iparams = clipperpy.invariants.EuclideanDistanceParams()
         iparams.sigma = self.config.invariant_sigma
@@ -80,8 +106,7 @@ class DataAssociationBlock:
 
     def certify_solution(
         self,
-        soln: np.ndarray | None = None,
-        inliers: torch.Tensor | None = None,
+        x: np.ndarray,
         cost: float = None,
         check_constraints: bool = False,
     ) -> AnalyticCenterResult:
@@ -89,26 +114,20 @@ class DataAssociationBlock:
 
         Parameters
         ----------
-        inliers : np.ndarray
-            Solution vector to certify.
+        x : np.ndarray
+            Solution vector for the max clique problem, of shape (N,).
+        cost : float, optional
+            Cost of the solution, defined as -x^T M x. If not provided, it will be computed from x and M.
+        check_constraints : bool, optional
+            If True, check that the solution x satisfies the constraints of the max clique problem. Default
 
         Returns
         -------
         result : AnalyticCenterResult
             Result of the certification process.
         """
-        assert (
-            soln is not None or inliers is not None
-        ), "Either x or inliers must be provided."
         # Retrieve the affinity matrix from the last forward pass
         M = self.get_affinity()
-
-        # Convert inliers to feasible solution if they are provided
-        if inliers is not None:
-            inliers_np = inliers.cpu().numpy()[:, None]
-            x = inliers_np / np.linalg.norm(inliers_np)
-        else:
-            x = soln
         # Check constraints
         if check_constraints:
             # Get the constraints for the max clique problem
@@ -221,55 +240,60 @@ class DataAssociationBlock:
         Args:
             inliers (torch.Tensor): Inlier mask for the matched keypoints, of shape (N,).
         Returns:
-            soln (np.ndarray): Solution vector for the max clique problem, of shape (N,).
+            soln (torch.Tensor): Solution vector for the max clique problem, of shape (N,).
             cost (float): Cost of the solution, defined as -x^T M x.
         """
         if self.M is None:
-            raise ValueError(
-                "Affinity matrix has not been set up. Call forward() first."
-            )
+            raise ValueError("Affinity matrix has not been set up.")
+
+        device = inliers.device
+        M = self.get_affinity(use_torch=True, device=device)
 
         # Select submatrix of M corresponding to inliers
-        inlier_idx = np.where(inliers.cpu().numpy())[0]
-        M_sub = self.M[np.ix_(inlier_idx, inlier_idx)]
+        inlier_idx = torch.nonzero(inliers.to(device).bool(), as_tuple=True)[0]
+        M_sub = M[inlier_idx][:, inlier_idx]
 
-        assert np.all(
+        assert torch.all(
             M_sub > 0
         ), "Cost submatrix contains non-positive elements. Inliers do not form a clique."
 
         # Power iteration to get Perron vector
-        v = np.ones(len(inlier_idx))
-        v /= np.linalg.norm(v)
+        v = torch.ones(len(inlier_idx), dtype=M.dtype, device=device)
+        v /= torch.linalg.norm(v)
         for i in range(self.config.clique_to_solution_iters):
             v_new = M_sub @ v
-            v_new /= np.linalg.norm(v_new)
-            if np.linalg.norm(v_new - v) < self.config.clique_to_solution_tol:
+            v_new /= torch.linalg.norm(v_new)
+            if torch.linalg.norm(v_new - v) < self.config.clique_to_solution_tol:
                 break
             v = v_new
         v = v_new
         cost = -float(v @ M_sub @ v)
 
         # Embed Perron vector into full solution
-        soln = np.zeros(self.M.shape[0])
+        soln = torch.zeros(M.shape[0], dtype=M.dtype, device=device)
         soln[inlier_idx] = v
         return soln, cost
 
-    def get_affinity(self):
+    def get_affinity(self, use_torch=False, device=torch.device("cpu")):
         """Get the affinity matrix from the CLIPPER block.
-        Warning: This should only be called after the forward pass, and will return the affinity matrix for the last pair of keypoints that were passed through the forward pass.
+        Warning: This should only be called after set_up_affinity_matrix has been called, otherwise the affinity matrix will be None.
         Returns:
             M (np.ndarray): Affinity matrix, of shape (N, N).
         """
         if self.M is None:
             raise ValueError(
-                "Affinity matrix has not been set up. Call forward() first."
+                "Affinity matrix has not been set up. Call set_up_affinity_matrix() first."
             )
+        if use_torch:
+            if self.M_torch is None:
+                self.M_torch = torch.from_numpy(self.M).to(device)
+            return torch.from_numpy(self.M).to(device)
         return self.M
 
     def run_ransac(
         self,
-        kpt_3D_src: torch.Tensor | None = None,
-        kpt_3D_trg: torch.Tensor | None = None,
+        kpt_3D_src: torch.Tensor,
+        kpt_3D_trg: torch.Tensor,
     ):
         """Forward pass through the RANSAC block. Keypoints with same index are assumed to be putative correspondences. The RANSAC block will output a mask of inliers for the matched keypoints.
         Args:
@@ -278,9 +302,83 @@ class DataAssociationBlock:
         Returns:
             inliers (torch.Tensor): Inlier mask for the matched keypoints, of shape (N,).
         """
-        raise NotImplementedError(
-            "RANSAC not implemented for base DataAssociationBlock class."
-        )
+        # Set up affinity matrix for max clique problem.
+        self.set_up_affinity_matrix(kpt_3D_src, kpt_3D_trg)
+        # track best inliers and cost
+        best_inliers = None
+        best_cost = np.inf
+        best_soln = None
+        with torch.no_grad():
+            for i in range(self.config.ransac_num_iterations):
+                # Randomly sample 3 points
+                idx = torch.randperm(kpt_3D_src.shape[1])[
+                    : self.config.ransac_num_sample_pts
+                ]
+                src_sample = kpt_3D_src[:, idx]
+                trg_sample = kpt_3D_trg[:, idx]
+
+                # Estimate pose using SVD
+                T_trg_src = estimate_pose_svd(
+                    src_sample,
+                    trg_sample,
+                )
+                # Transform source points to target frame
+                kpt_3D_src_inTrg = T_trg_src @ kpt_3D_src
+                # Compute distances between transformed source points and target points
+                distances = torch.norm(
+                    kpt_3D_src_inTrg[:3, :] - kpt_3D_trg[:3, :], dim=0
+                )
+                # Determine inliers based on distance threshold
+                inliers = distances < self.config.ransac_inlier_threshold
+                # Convert RANSAC inliers to solution vector for the max clique problem
+                soln, cost = self.inliers_to_solution(inliers)
+                # Update best inliers if cost is better
+                if cost < best_cost or best_inliers is None:
+                    best_cost = cost
+                    best_inliers = inliers
+                    best_soln = soln
+        return best_inliers, best_soln, best_cost
+
+
+def estimate_pose_svd(keypoints_3D_src, keypoints_3D_trg):
+    """Estimate the relative pose between the source and target frames using SVD.
+
+    Args:
+        keypoints_3D_src (torch.Tensor): 3D point coordinates of keypoints from source frame, of shape (4, N).
+        keypoints_3D_trg (torch.Tensor): 3D point coordinates of keypoints from target frame, of shape (4, N).
+    Returns:
+        T_trg_src (torch.Tensor): Relative transform from the source to the target frame,
+    """
+
+    # Compute centroids (elementwise multiplication/division)
+    n_points = keypoints_3D_src.shape[1]
+    centroid_src = (
+        torch.sum(keypoints_3D_src[0:3, :], dim=1, keepdim=True) / n_points
+    )  # 3x1
+    centroid_trg = (
+        torch.sum(keypoints_3D_trg[0:3, :], dim=1, keepdim=True) / n_points
+    )  # 3x1
+    # Compute centered coordinates
+    src_centered = keypoints_3D_src[0:3, :] - centroid_src  # 3xN
+    trg_centered = keypoints_3D_trg[0:3, :] - centroid_trg
+    # Compute rotation and translation (T_trg_src in sensor frame)
+    H = trg_centered @ src_centered.transpose(1, 0).contiguous()  # 3x3
+    U, S, V = torch.svd(H)
+    det_UV = torch.det(U) * torch.det(V)
+    diag = torch.diag_embed(torch.Tensor([1.0, 1.0, det_UV]).type_as(V))  # 3x3
+    R_trg_src = U @ diag @ V.transpose(1, 0)  # 3x3
+    # Translation from trg to src given in src frame
+    # NOTE: Uses the fact that the centroids should be coincident after rotation.
+    t_src_trg_intrg = centroid_trg - R_trg_src @ centroid_src  # 3x1
+
+    # Create translation matrix
+    zeros = torch.zeros(1, 3).type_as(V)  # 1x3
+    one = torch.ones(1, 1).type_as(V)  # 1x1
+    trans_cols = torch.cat([t_src_trg_intrg, one], dim=0)  # 4x1
+    rot_cols = torch.cat([R_trg_src, zeros], dim=0)  # 4x3
+    T_trg_src = torch.cat([rot_cols, trans_cols], dim=1)  # 4x4
+
+    return T_trg_src
 
 
 def get_maxclique_sdp_constraints(M: np.ndarray):
--- tests/test_data_association.py
+++ tests/test_data_association.py
@@ -6,14 +6,14 @@ import numpy as np
 ROOT = Path(__file__).resolve().parents[1]
 SRC = ROOT / "src"
 sys.path.insert(0, str(SRC))
-config_file = ROOT / "configs" / "test_config.yaml"
+config_file = ROOT / "configs" / "stereo_pipeline" / "test_config.yaml"
 
-from stereo_loc.DataAssociationBlocks import DataAssociationBlock
+from stereo_loc.DataAssociationBlocks import DataAssociationBlock, estimate_pose_svd
 from stereo_loc.StereoPipeline import load_config
 from fixtures import bunny_stereo_synthetic  # noqa: F401
 
 
-def test_clipper_block(bunny_stereo_synthetic):
+def test_data_association(bunny_stereo_synthetic):
 
     points_c1 = bunny_stereo_synthetic["points_1"]
     points_c2 = bunny_stereo_synthetic["points_2"]
@@ -24,8 +24,8 @@ def test_clipper_block(bunny_stereo_synthetic):
     data_association_config = config.data_association_config
     data_association_config.invariant_epsilon = 0.002
     data_association_config.invariant_sigma = 0.001
-    clipper_block = DataAssociationBlock(data_association_config)
-    inliers, soln = clipper_block.run_clipper(
+    data_association = DataAssociationBlock(data_association_config)
+    inliers, soln = data_association.run_clipper(
         torch.from_numpy(points_c1.T).float().to("cpu"),  # (4,N)
         torch.from_numpy(points_c2.T).float().to("cpu"),  # (4,N)
     )
@@ -34,7 +34,7 @@ def test_clipper_block(bunny_stereo_synthetic):
         torch.sum(inliers) >= points_c1.shape[0] - n_outliers
     ), f"Expected at least {points_c1.shape[0] - n_outliers} inliers, but got {torch.sum(inliers)} inliers out of {points_c1.shape[0]} total points"
     # Test certification
-    result = clipper_block.certify_solution(soln=soln, check_constraints=True)
+    result = data_association.certify_solution(soln, check_constraints=True)
     assert result.certified, "Certifier could not certify solution"
 
 
@@ -48,19 +48,19 @@ def test_inliers_to_soln(bunny_stereo_synthetic):
     data_association_config = config.data_association_config
     data_association_config.invariant_epsilon = 0.02
     data_association_config.invariant_sigma = 0.01
-    clipper_block = DataAssociationBlock(data_association_config)
-    inliers, soln = clipper_block.run_clipper(
+    data_association = DataAssociationBlock(data_association_config)
+    inliers, soln = data_association.run_clipper(
         torch.from_numpy(points_c1.T).float().to("cpu"),  # (4,N)
         torch.from_numpy(points_c2.T).float().to("cpu"),  # (4,N)
     )
-    soln_from_inliers, cost = clipper_block.inliers_to_solution(inliers=inliers)
+    soln_from_inliers, cost = data_association.inliers_to_solution(inliers=inliers)
     # assert that the solutions are the same
     assert np.linalg.allclose(
         soln, soln_from_inliers, atol=1e-7
     ), "Solutions from run_clipper and inliers_to_soln do not match"
 
 
-def test_clipper_block_threshold(bunny_stereo_synthetic):
+def test_data_association_threshold(bunny_stereo_synthetic):
     points_c1 = bunny_stereo_synthetic["points_1"]
     points_c2 = bunny_stereo_synthetic["points_2"]
     n_outliers = bunny_stereo_synthetic["n_outliers"]
@@ -72,8 +72,8 @@ def test_clipper_block_threshold(bunny_stereo_synthetic):
     data_association_config.invariant_sigma = 0.01
     data_association_config.unweighted = True  # Enable thresholding
 
-    clipper_block = DataAssociationBlock(data_association_config)
-    inliers, x = clipper_block.run_clipper(
+    data_association = DataAssociationBlock(data_association_config)
+    inliers, x = data_association.run_clipper(
         torch.from_numpy(points_c1.T).float().to("cpu"),  # (4,N)
         torch.from_numpy(points_c2.T).float().to("cpu"),  # (4,N)
     )
@@ -82,7 +82,8 @@ def test_clipper_block_threshold(bunny_stereo_synthetic):
         torch.sum(inliers) >= points_c1.shape[0] - n_outliers
     ), f"Expected at least {points_c1.shape[0] - n_outliers} inliers, but got {torch.sum(inliers)} inliers out of {points_c1.shape[0]} total points"
     # Test certification
-    result = clipper_block.certify_solution(inliers=inliers, check_constraints=True)
+
+    result = data_association.certify_solution(x, check_constraints=True)
     assert result.certified, "Certifier could not certify solution"
 
 
@@ -97,9 +98,9 @@ def test_clipper_sdp(bunny_stereo_synthetic):
     data_association_config = config.data_association_config
     data_association_config.invariant_epsilon = 0.002
     data_association_config.invariant_sigma = 0.001
-    clipper_block = DataAssociationBlock(data_association_config)
+    data_association = DataAssociationBlock(data_association_config)
     # Run the CLIPPER SDP block to get inliers and the solution u
-    inliers, u = clipper_block.run_sdp(
+    inliers, u = data_association.run_sdp(
         torch.from_numpy(points_c1.T).float().to("cpu"),  # (4,N)
         torch.from_numpy(points_c2.T).float().to("cpu"),  # (4,N)
     )
@@ -108,5 +109,57 @@ def test_clipper_sdp(bunny_stereo_synthetic):
         torch.sum(inliers) >= points_c1.shape[0] - n_outliers
     ), f"Expected at least {points_c1.shape[0] - n_outliers} inliers, but got {torch.sum(inliers)} inliers out of {points_c1.shape[0]} total points"
     # Test certification
-    result = clipper_block.certify_solution(soln=u, check_constraints=True)
+    result = data_association.certify_solution(u, check_constraints=True)
+    assert result.certified, "Certifier could not certify solution"
+
+
+def test_svd_estimator(bunny_stereo_synthetic):
+    points_c1 = bunny_stereo_synthetic["points_1"]
+    points_c2 = bunny_stereo_synthetic["points_2"]
+    T_12 = bunny_stereo_synthetic["T_12"]
+    T_trg_src_gt = torch.from_numpy(np.linalg.inv(T_12))
+
+    # Remove outliers from the points
+    n_outliers = bunny_stereo_synthetic["n_outliers"]
+    points_c1 = points_c1[: points_c1.shape[0] - n_outliers, :]
+    points_c2 = points_c2[: points_c2.shape[0] - n_outliers, :]
+
+    # Run the SVD estimator to get the transformation matrix
+    T_trg_src = estimate_pose_svd(
+        torch.from_numpy(points_c1.T).float().to("cpu"),  # (4,N)
+        torch.from_numpy(points_c2.T).float().to("cpu"),  # (4,N)
+    )
+
+    assert T_trg_src.shape == (
+        4,
+        4,
+    ), "SVD estimator did not return a valid transformation matrix"
+    assert np.allclose(
+        T_trg_src.numpy(), T_trg_src_gt, atol=1e-6
+    ), "SVD estimator did not return the correct transformation matrix"
+
+
+def test_ransac_cert(bunny_stereo_synthetic):
+    points_c1 = bunny_stereo_synthetic["points_1"]
+    points_c2 = bunny_stereo_synthetic["points_2"]
+    n_outliers = bunny_stereo_synthetic["n_outliers"]
+
+    # Instantiate CLIPPER block
+    config = load_config(config_file)
+    data_association_config = config.data_association_config
+    data_association_config.invariant_epsilon = 0.002
+    data_association_config.invariant_sigma = 0.001
+    data_association = DataAssociationBlock(data_association_config)
+    # Run the CLIPPER SDP block to get inliers and the solution u
+    inliers, x_torch, cost = data_association.run_ransac(
+        torch.from_numpy(points_c1.T).float().to("cpu"),  # (4,N)
+        torch.from_numpy(points_c2.T).float().to("cpu"),  # (4,N)
+    )
+    # assert that the original sampled points are inliers and the synthetic outliers are rejected
+    assert (
+        torch.sum(inliers) >= points_c1.shape[0] - n_outliers
+    ), f"Expected at least {points_c1.shape[0] - n_outliers} inliers, but got {torch.sum(inliers)} inliers out of {points_c1.shape[0]} total points"
+    # Test certification
+    x = x_torch.cpu().numpy()
+    result = data_association.certify_solution(x, check_constraints=True)
     assert result.certified, "Certifier could not certify solution"
