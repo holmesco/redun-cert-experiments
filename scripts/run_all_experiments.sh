166011863444467d7674cc7ca3f9af6c4b2d60cd
cholmes <connor.holmes@mail.utoronto.ca>
2026-09-02 19:57:28 +0000

    update experiment run script. remove dead code.

--- scripts/run_all_experiments.sh
+++ scripts/run_all_experiments.sh
@@ -3,6 +3,8 @@ cd /workspace/experiments
 # activate env
 . .venv/bin/activate
 
+# STANFORD BUNNY EXPERIMENTS
+# ==========================
 # Run parameter sweep experiments 
 python src/experiments/stanford_bunny_experiments.py invariant_sweep.yaml
 
@@ -11,3 +13,14 @@ python src/experiments/stanford_bunny_experiments.py invariant_sweep.yaml
 python src/experiments/stanford_bunny_experiments.py timing_sweep_outlier_ratio.yaml
 # Sweep associations
 python src/experiments/stanford_bunny_experiments.py timing_sweep_num_assoc.yaml
+
+# POSE REGISTRATION EXPERIMENTS
+# =============================
+# Make Setup plots
+python src/experiments/pose_reg_experiment.py pose_reg_setup.yaml
+# Run sweep over different distances
+python src/experiments/pose_reg_experiment.py distance_sweep.yaml
+
+# STEREO PIPELINE EXPERIMENTS
+# ===========================
+python src/experiments/stereo_pipeline_experiment.py MH01e_clipper.yaml
